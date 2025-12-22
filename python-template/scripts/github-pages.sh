2f2422615362764be49f726f094019a69d157336
Filippo Mameli <mameli93@gmail.com>
2025-12-23 00:31:19 +0100

    fix: doc branch name

--- python-template/scripts/github-pages.sh
+++ python-template/scripts/github-pages.sh
@@ -9,25 +9,12 @@ echo "Current version: ${VERSION}"
 echo "Generate full changelog"
 uv run cz changelog
 
-current_branch="$(git rev-parse --abbrev-ref HEAD)"
-
-# Create orphan branch "gh-pages"
-git checkout --orphan gh-pages
-git reset --hard
-git clean -fdx
-
-# Push branch to remote
-git push -u origin gh-pages
-
-# Return to original branch
-git switch "${current_branch}"
-
 # This command is mandatory to have gh-pages branch updated.
 echo "List documentation versions..."
 uv run mike list
 
 echo "Deploying ${VERSION} as 'latest' version using mike..."
-uv run mike deploy --branch docs --deploy-prefix public --push "${VERSION}" latest --update-aliases --alias-type=copy
+uv run mike deploy --branch gh-pages --deploy-prefix public --push "${VERSION}" latest --update-aliases --alias-type=copy
 echo "Ensure default version is set to 'latest'..."
-uv run mike set-default --branch docs --deploy-prefix public --push latest
+uv run mike set-default --branch gh-pages --deploy-prefix public --push latest
 echo 'OK'
--- python-template/scripts/init-docs.sh
+++ python-template/scripts/init-docs.sh
@@ -8,8 +8,14 @@ git checkout --orphan gh-pages
 git reset --hard
 git clean -fdx
 
+uv venv
+uv pip install pre-commit
+uv run pre-commit install || true
+
+PRE_COMMIT_ALLOW_NO_CONFIG=1 git commit -m "feat: Initial commit on docs branch" --no-verify
+
 # Push branch to remote
-git push -u origin gh-pages
+git push -u origins gh-pages 
 
 # Return to original branch
 git switch "${current_branch}"
